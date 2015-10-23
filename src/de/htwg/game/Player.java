3b5a90960b2264db897302e44333eb258c8f8227
luluschi <lukas.luschin@gmail.com>
2015-10-23 09:43:27 +0200

    Added minor functionality and JavaDoc comments

--- src/de/htwg/game/Player.java
+++ src/de/htwg/game/Player.java
@@ -10,27 +10,55 @@ public class Player {
     private int score;
     private ArrayList<Tile> hand;
 
+    /**
+     *
+     * @param name The name of the player
+     */
     public Player(String name) {
         this.name = name;
-        this.reset();
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public int getScorte() {
-        return this.score;
-    }
-
-    public ArrayList<Tile> getHand() {
-        return this.hand;
-    }
-
-    private final void reset() {
         this.score = 0;
         this.hand = new ArrayList<Tile>();
     }
 
+    /**
+     * Adds a Tile to the player's hand
+     * @param t Tile to add
+     */
+    public void addTileToHand(Tile t) {
 
+    }
+
+    /**
+     * Adds a given number of points to the player's score
+     * @param i Value to add to score. Has to be greater than 0
+     */
+    public void addScore(int i) {
+        if (i > 0) {
+            this.score += i;
+        }
+    }
+
+    /**
+     *
+     * @return the name of the player
+     */
+    public String getName() {
+        return this.name;
+    }
+
+    /**
+     *
+     * @return the current score of the player
+     */
+    public int getScore() {
+        return this.score;
+    }
+
+    /**
+     *
+     * @return a copy of the player's hand
+     */
+    public ArrayList<Tile> getHand() {
+        return new ArrayList<Tile>(this.hand);
+    }
 }
