fe98bb0fce3363225720a083ea5bbd04b49b9601
luluschi <lukas.luschin@gmail.com>
2015-10-23 09:18:04 +0200

    Added Player class to project

---
+++ src/game/Player.java
@@ -0,0 +1,36 @@
+package game;
+
+import java.util.ArrayList;
+
+/**
+ * Created by luluschi on 23.10.2015.
+ */
+public class Player {
+    private String name;
+    private int score;
+    private ArrayList<Tile> hand;
+
+    public Player(String name) {
+        this.name = name;
+        this.reset();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getScorte() {
+        return this.score;
+    }
+
+    public ArrayList<Tile> getHand() {
+        return this.hand;
+    }
+
+    private final void reset() {
+        this.score = 0;
+        this.hand = new ArrayList<Tile>();
+    }
+
+
+}
