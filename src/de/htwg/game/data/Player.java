7ec7db8d81a8d309ea088e78a6dd96ed884f93fa
luluschi <lukas.luschin@gmail.com>
2015-10-23 11:53:25 +0200

    created new architecture for MVC

---
+++ src/de/htwg/game/data/Player.java
@@ -0,0 +1,66 @@
+package de.htwg.game.data;
+
+import java.util.ArrayList;
+
+/**
+ * Created by luluschi on 23.10.2015.
+ */
+public class Player {
+    private String name;
+    private int score;
+    private ArrayList<Tile> hand;
+
+    /**
+     *
+     * @param name The name of the player
+     */
+    public Player(String name) {
+        this.name = name;
+        this.score = 0;
+        this.hand = new ArrayList<Tile>();
+    }
+
+    /**
+     * Adds a Tile to the player's hand
+     * @param t Tile to add
+     */
+    public void addTileToHand(Tile t) {
+        if(this.hand.size() < 6) {
+            this.hand.add(t);
+        }
+    }
+
+    /**
+     * Adds a given number of points to the player's score
+     * @param i Value to add to score. Has to be greater than 0
+     */
+    public void addScore(int i) {
+        if (i > 0) {
+            this.score += i;
+        }
+    }
+
+    /**
+     *
+     * @return the name of the player
+     */
+    public String getName() {
+        return this.name;
+    }
+
+    /**
+     *
+     * @return the current score of the player
+     */
+    public int getScore() {
+        return this.score;
+    }
+
+    /**
+     *
+     * @return a copy of the player's hand
+     */
+    public ArrayList<Tile> getHand() {
+        return new ArrayList<Tile>(this.hand);
+    }
+}
---
+++ src/de/htwg/game/engine/QGame.java
@@ -0,0 +1,20 @@
+package de.htwg.game.engine;
+
+import de.htwg.game.data.Tile;
+
+/**
+ * Created by niels on 23.10.2015.
+ */
+public class QGame {
+
+    private static final Tile[][] BOARD = new Tile[101][101];
+
+    public static void main(String[] args) {
+
+    }
+
+    private void init() {
+
+    }
+
+}
