ad16ba1f8eab14427eea64c00c1fcc2da89a5054
niels <nielsboecker@gmail.com>
2015-10-23 09:29:21 +0200

    test

--- src/game/Tile.java
+++ src/game/Tile.java
@@ -4,4 +4,31 @@ package game;
  * Created by niels on 23.10.2015.
  */
 public class Tile {
+
+    final private String color;
+    final private String shape;
+
+    public Tile(String color, String shape) {
+        this.color = color;
+        this.shape = shape;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getShape() {
+        return shape;
+    }
+
+    @Override
+    public String toString() {
+        return "Tile {" +
+                "color='" + color + '\'' +
+                ", shape='" + shape + '\'' +
+                '}';
+    }
+
 }
+
+
