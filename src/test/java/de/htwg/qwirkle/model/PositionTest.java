44f6e43470f21d09bb49718f3a3c73c7e5e3feba
niels <nielsboecker@gmail.com>
2016-01-08 11:08:04 +0100

    wrote Position tests

--- src/test/java/de/htwg/qwirkle/model/PositionTest.java
+++ src/test/java/de/htwg/qwirkle/model/PositionTest.java
@@ -1,41 +1,54 @@
 package de.htwg.qwirkle.model;
 
+import javafx.geometry.Pos;
+import org.junit.Before;
 import org.junit.Test;
+import de.htwg.qwirkle.model.Grid.*;
 
 import static org.junit.Assert.*;
 
-/**
- * Created by niels on 08.01.2016.
- */
 public class PositionTest {
+
+    Position position;
+
+    @Before
+    public void setUp() {
+        position = new Position(10, 10);
+    }
 
     @Test
     public void testGetRow() throws Exception {
-        // TODO
+        assertEquals(10, position.getRow());
     }
 
     @Test
     public void testGetCol() throws Exception {
-        // TODO
+        assertEquals(10, position.getCol());
     }
 
     @Test
     public void testSetPosition() throws Exception {
-        // TODO
+        position.setPosition(20, 20);
+        assertEquals(20, position.getCol());
+        assertEquals(20, position.getRow());
     }
 
     @Test
     public void testEquals() throws Exception {
-        // TODO
+        Position pos1 = new Position(1, 1);
+        Position pos2 = new Position(1, 1);
+        assertEquals(pos1, pos2);
     }
 
     @Test
     public void testHashCode() throws Exception {
-        // TODO
+        assertEquals(220, position.hashCode());
     }
 
     @Test
     public void testToString() throws Exception {
-        // TODO
+        // the getClass().getName() method apparently uses '$' to show inner class
+        String testOutput = "de.htwg.qwirkle.model.Grid$Position[row=10,col=10]";
+        assertEquals(testOutput, position.toString());
     }
 }
